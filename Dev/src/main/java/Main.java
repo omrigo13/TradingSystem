b0f1378f791f0df23a9d344ff7d5eab9ee3ca8ad
nono20794 <chardon@post.bgu.ac.il>
2021-03-31 18:39:26 +0300

    change store, inventory and item methods to work with itemID,

--- Dev/src/main/java/Main.java
+++ Dev/src/main/java/Main.java
@@ -5,21 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world");
-        ConcurrentLinkedQueue<Integer> l1=new ConcurrentLinkedQueue<>();
-        l1.add(2);
-        l1.add(3);
-        l1.add(7);
-        ConcurrentLinkedQueue<Integer> l2=new ConcurrentLinkedQueue<>();
-        l2.add(2);
-        l2.add(7);
-        l2.add(4);
-        ConcurrentLinkedQueue<Integer> l3=new ConcurrentLinkedQueue<>();
-        l3.add(2);
-        l3.add(3);
-        l3.add(7);
-        l1.retainAll(l2);
-        l1.retainAll(l3);
-
-        System.out.println(l1);
+ 
     }
 }
