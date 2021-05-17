189c85296aa17882c68aace011ff9dd56c2d8008
Omri Goldberg <goldomri@post.bgu.ac.il>
2021-05-17 09:42:09 +0300

    updates for support of external services

--- Dev/src/main/java/user/User.java
+++ Dev/src/main/java/user/User.java
@@ -65,7 +65,9 @@ public class User {
             return;
         paymentData.setPaymentValue(totalPrice);
         try {
+            paymentSystem.connect();
             paymentSystem.pay(paymentData);
+            deliverySystem.connect();
             deliverySystem.deliver(deliveryData);
         }
         catch (PaymentSystemException pe) {
