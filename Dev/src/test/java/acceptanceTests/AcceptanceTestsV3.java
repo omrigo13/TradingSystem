7a65d3accfec7312bf0d25f9a6079f7620cb16ae
Omri Goldberg <goldomri@post.bgu.ac.il>
2021-05-19 15:00:49 +0300

    start working on acceptance tests v3

---
+++ Dev/src/test/java/acceptanceTests/AcceptanceTestsV3.java
@@ -0,0 +1,11 @@
+package acceptanceTests;
+
+public class AcceptanceTestsV3 {
+
+    //TODO check addItemToBasketByOffer
+    //TODO check getOffersByStore
+    //TODO check approveOffer
+    //TODO check getTotalIncomeByStorePerDay
+    //TODO check getTotalIncomeByAdminPerDay
+
+}
