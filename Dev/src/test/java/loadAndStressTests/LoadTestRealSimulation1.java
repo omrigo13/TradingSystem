9c612b45c2b2c535d5e74c11b75fd19b4ba7d160
Omri Goldberg <goldomri@post.bgu.ac.il>
2021-06-09 15:56:26 +0300

    add more load tests

---
+++ Dev/src/test/java/loadAndStressTests/LoadTestRealSimulation1.java
@@ -0,0 +1,86 @@
+package loadAndStressTests;
+
+import authentication.UserAuthentication;
+import exceptions.InvalidActionException;
+import exceptions.ItemNotFoundException;
+import exceptions.ItemNotPurchasedException;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import service.TradingSystemServiceImpl;
+import store.Store;
+import tradingSystem.TradingSystemBuilder;
+import tradingSystem.TradingSystemImpl;
+import user.AdminPermission;
+import user.Subscriber;
+import user.User;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class LoadTestRealSimulation1 {
+
+    private TradingSystemServiceImpl tradingSystemService;
+    private final String userName = "Admin";
+    private final String password = "123";
+    private final ConcurrentHashMap<String, Subscriber> subscribers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, User> connections = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Integer, Store> stores = new ConcurrentHashMap<>();
+    private final UserAuthentication auth = new UserAuthentication();
+    private final Subscriber admin = new Subscriber(0, userName);
+    private final AtomicInteger subscriberId = new AtomicInteger(0);
+    private long start;
+
+    @BeforeClass
+    void setUp() throws InvalidActionException {
+        MockitoAnnotations.openMocks(this);
+        MockitoAnnotations.openMocks(this);
+        auth.register(userName, password);
+        admin.addPermission(AdminPermission.getInstance());
+        subscribers.put(userName, admin);
+        tradingSystemService = new TradingSystemServiceImpl(new TradingSystemImpl(new TradingSystemBuilder().setUserName(userName)
+                .setPassword(password)
+                .setSubscribers(subscribers)
+                .setConnections(connections)
+                .setStores(stores)
+                .setAuth(auth)
+                .build()));
+        start = System.nanoTime();
+        String conn;
+        for(int i = 0; i < 70; i++){
+            conn = tradingSystemService.connect();
+            tradingSystemService.register("s" + i, "1234");
+            tradingSystemService.login(conn, "s" + i, "1234");
+        }
+        for(int i = 0; i < 30; i++){
+            conn = tradingSystemService.connect();
+            tradingSystemService.login(conn, "s" + i, "1234");
+            String store = tradingSystemService.openNewStore(conn, "eBay" + i);
+            tradingSystemService.addProductToStore(conn, store, "bisli", "snacks", "sub1", 500, 5.5);
+        }
+    }
+
+    @Test(threadPoolSize = 10, invocationCount = 100, timeOut = 10000)
+    public void test() throws InvalidActionException {
+        String conn = tradingSystemService.connect();
+        int id = subscriberId.getAndIncrement();
+        if(id < 70)
+            tradingSystemService.login(conn, "s" + id, "1234");
+        if(id < 10) {
+            tradingSystemService.appointStoreOwner(conn, "s" + (id + 40), String.valueOf(id));
+        }
+        else if(id < 30) {
+            tradingSystemService.addProductToStore(conn, String.valueOf(id), "bamba" + id, "snacks", "sub1", 500, 5.5);
+        }
+        else if(id < 50) {
+            try { tradingSystemService.addItemToBasket(conn, String.valueOf(id - 30), "1", 5); }
+            catch (ItemNotFoundException e){ tradingSystemService.addItemToBasket(conn, String.valueOf(id - 30), "0", 5); }
+            tradingSystemService.purchaseCart(conn, "1", 1, 2022, "1", "1", "1", "1", "1", "1", "1", 1);
+            try {tradingSystemService.writeOpinionOnProduct(conn, String.valueOf(id - 30), "0", "description"); }
+            catch (ItemNotPurchasedException e) {tradingSystemService.writeOpinionOnProduct(conn, String.valueOf(id - 30), "1", "description"); }
+            catch (NullPointerException e) {
+                // nothing
+            }
+        }
+    }
+}
---
+++ Dev/src/test/java/loadAndStressTests/LoadTestRealSimulation2.java
@@ -0,0 +1,83 @@
+package loadAndStressTests;
+
+import authentication.UserAuthentication;
+import exceptions.InvalidActionException;
+import exceptions.ItemNotFoundException;
+import exceptions.ItemNotPurchasedException;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import service.TradingSystemServiceImpl;
+import store.Store;
+import tradingSystem.TradingSystemBuilder;
+import tradingSystem.TradingSystemImpl;
+import user.AdminPermission;
+import user.Subscriber;
+import user.User;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class LoadTestRealSimulation2 {
+
+    private TradingSystemServiceImpl tradingSystemService;
+    private final String userName = "Admin";
+    private final String password = "123";
+    private final ConcurrentHashMap<String, Subscriber> subscribers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, User> connections = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Integer, Store> stores = new ConcurrentHashMap<>();
+    private final UserAuthentication auth = new UserAuthentication();
+    private final Subscriber admin = new Subscriber(0, userName);
+    private final AtomicInteger subscriberId = new AtomicInteger(0);
+    private long start;
+
+    @BeforeClass
+    void setUp() throws InvalidActionException {
+        MockitoAnnotations.openMocks(this);
+        MockitoAnnotations.openMocks(this);
+        auth.register(userName, password);
+        admin.addPermission(AdminPermission.getInstance());
+        subscribers.put(userName, admin);
+        tradingSystemService = new TradingSystemServiceImpl(new TradingSystemImpl(new TradingSystemBuilder().setUserName(userName)
+                .setPassword(password)
+                .setSubscribers(subscribers)
+                .setConnections(connections)
+                .setStores(stores)
+                .setAuth(auth)
+                .build()));
+        start = System.nanoTime();
+        String conn;
+        for(int i = 0; i < 70; i++){
+            conn = tradingSystemService.connect();
+            tradingSystemService.register("s" + i, "1234");
+            tradingSystemService.login(conn, "s" + i, "1234");
+        }
+        for(int i = 0; i < 30; i++){
+            conn = tradingSystemService.connect();
+            tradingSystemService.login(conn, "s" + i, "1234");
+            String store = tradingSystemService.openNewStore(conn, "eBay" + i);
+            tradingSystemService.addProductToStore(conn, store, "bisli", "snacks", "sub1", 500, 5.5);
+        }
+    }
+
+    @Test(threadPoolSize = 10, invocationCount = 100, timeOut = 5000)
+    public void test() throws InvalidActionException {
+        String conn = tradingSystemService.connect();
+        int id = subscriberId.getAndIncrement();
+        if(id < 70)
+            tradingSystemService.login(conn, "s" + id, "1234");
+        if(id < 10) {
+            tradingSystemService.appointStoreOwner(conn, "s" + (id + 40), String.valueOf(id));
+            String conn2 = tradingSystemService.connect();
+            tradingSystemService.login(conn2, "s" + (id + 40), "1234");
+            tradingSystemService.appointStoreManager(conn2, "s" + (id + 60), String.valueOf(id));
+            tradingSystemService.allowManagerToUpdateProducts(conn2, String.valueOf(id), "s" + (id + 60));
+            String conn3 = tradingSystemService.connect();
+            tradingSystemService.login(conn3, "s" + (id + 60), "1234");
+            tradingSystemService.updateProductDetails(conn3, String.valueOf(id), "0", null, null, 3.5);
+        }
+        tradingSystemService.addItemToBasket(conn, "0", "0", 3);
+    }
+}
